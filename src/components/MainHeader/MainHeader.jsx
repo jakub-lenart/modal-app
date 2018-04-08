48be8049128d43ee2821d33b11afe00aed18f386
Jakub Lenart <jakub@lenart.tech>
2018-04-08 13:19:49 +0200

    run eject script and add css modules support

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './MainHeader.css';
+import Styles from './MainHeader.css';
 
 class MainHeader extends Component {
   render() {
     return (
-      <header className="main-header">
+      <header className={Styles.MainHeader}>
         <div className="container">
           <h1>Take good care of your beloved<br />car and purchase an insurance!</h1>
           <button>See more</button>
